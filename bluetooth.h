4cf2e408eaa2c796c241dff060cff88eaf36b744
U_Pong <104551700+UPoong@users.noreply.github.com>
2024-11-11 17:11:35 +0900

    Add files via upload

---
+++ bluetooth.h
@@ -0,0 +1,16 @@
+/*
+ * bluetooth.h
+ *
+ *  Created on: Oct 11, 2024
+ *      Author: jsmok
+ */
+
+#ifndef INC_BLUETOOTH_H_
+#define INC_BLUETOOTH_H_
+
+#include "top.h"
+
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart );
+//void UART6INIT(void);
+
+#endif /* INC_BLUETOOTH_H_ */
